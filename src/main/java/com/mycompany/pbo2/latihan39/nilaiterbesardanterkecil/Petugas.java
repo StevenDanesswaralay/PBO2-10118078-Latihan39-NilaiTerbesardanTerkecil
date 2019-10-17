91f5441cf924da87ee1006a28538382426640c4d
Steven Danesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-10-17 09:46:50 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author 
+ * Nama     : Steven Danesswaralay
+ * Kelas    : PBO2
+ * Nim      : 10118078
+ * Deskripsi Tugas : Nilai Terbesar dan Terkecil
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
